0055feec568fa5419b8eee5c804a639a93d842cb
Hung Anh <anhlhpl52119@gmail.com>
2023-08-24 22:18:50 +0700

    feat: init

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_APP_TITLE: string
+    readonly VITE_BASE_API_URL: string
+    readonly VITE_API_AUTH_PREFIX: string
+    readonly VITE_API_VERSION_PREFIX: string
+    readonly VITE_IS_TEST: string
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
