af297234d54b7eb905005a1153e4bc9ea82ed2fe
Hung Anh <anhlhpl52119@gmail.com>
2023-08-26 22:58:05 +0700

    feat: add custom route types

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { type RouteComponent, createRouter, createWebHistory } from 'vue-router';
+import MainLayout from '@/layout/index.vue';
+
+interface CustomRoute {
+  path: string
+  name?: string
+  component: RouteComponent
+  children?: CustomRoute[]
+  meta?: {
+    icon?: string
+    title?: string
+    permitRoles?: any[]
+  }
+}
+
+const routes2: CustomRoute[] = [{
+  path: '/',
+  component: MainLayout,
+  children: [
+    {
+      path: '/',
+      component: () => import('@/views/home.vue'),
+    },
+    {
+      path: '/dev',
+      component: () => import('@/views/dev.vue'),
+    },
+  ],
+}];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      component: import('@/layout/index.vue'),
-      children: [
-        {
-          path: '/',
-          component: import('@/views/home.vue'),
-        },
-        {
-          path: '/dev',
-          component: import('@/views/dev.vue'),
-        },
-      ],
-    },
-    {
-      path: '/dev',
-      name: 'dev',
-      component: import('@/views/dev.vue'),
-    },
+    ...routes2,
   ],
 });
 
