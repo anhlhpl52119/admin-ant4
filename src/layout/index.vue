8032e00e6a0e38bb34c61ac9fc8ba113478bb091
Hung Anh <anhlhpl52119@gmail.com>
2023-08-28 20:44:16 +0700

    feat: add dynamic menu

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,46 +1,16 @@
 <template>
   <ALayout class="min-h-screen">
     <div class="bg-white m-24 h-40" />
-    <ALayoutSider v-model:collapsed="collapsed" collapsible class="bg-white">
-      <AMenu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <AMenuItem key="1" class="flex-center">
-          <PieChartOutlined />
-          <span>Option 1 controller for each other</span>
-        </AMenuItem>
-        <ASubMenu key="sub1">
-          <template #title>
-            <UserOutlined />
-            <span>User</span>
-          </template>
-          <AMenuItem key="3" @click="$router.push('/dev')">
-            Tom
-          </AMenuItem>
-          <AMenuItem key="4" @click="$router.push('/')">
-            Bill
-          </AMenuItem>
-          <AMenuItem key="5">
-            Alex
-          </AMenuItem>
-          <ASubMenu key="sub15">
-            <template #title>
-              <UserOutlined />
-              <span>User</span>
-            </template>
-            <AMenuItem key="31">
-              Tom
-            </AMenuItem>
-            <AMenuItem key="41">
-              Bill
-            </AMenuItem>
-            <AMenuItem key="51">
-              Alex
-            </AMenuItem>
-          </ASubMenu>
-        </ASubMenu>
+    <ALayoutSider v-model:collapsed="collapsed" collapsible :width="250">
+      <AMenu v-model:openKeys="activeKey" :selected-keys="selectedKeys" theme="dark" mode="inline">
+        <template v-for="item in userStore.userMenu" :key="item.name">
+          <CustomMenuItem v-if="!item.children || item.children.length === 0" :data="item" />
+          <CustomSubMenu v-else :data="item" />
+        </template>
       </AMenu>
     </ALayoutSider>
     <ALayout>
-      <!-- <header class="h-64 bg-white" /> -->
+      <header class="h-64 bg-white" />
       <ALayoutContent class="m-16">
         <div class="card bg-white min-h-360">
           <PageContent />
@@ -54,12 +24,22 @@
 </template>
 
 <script lang="ts" setup>
-import {
-  PieChartOutlined, UserOutlined,
-} from '@ant-design/icons-vue';
-import { ref } from 'vue';
+import CustomMenuItem from './left-menu/CustomMenuItem.vue';
+import CustomSubMenu from './left-menu/CustomSubMenu.vue';
 import PageContent from '@/layout/page-content/index.vue';
+import { useUserStore } from '@/stores/user.store';
+import { ERouteName } from '@/enums/router.enum';
 
+const routes = useRoute();
+const userStore = useUserStore();
 const collapsed = ref<boolean>(false);
-const selectedKeys = ref<string[]>(['1']);
+const activeKey = ref<string[]>([ERouteName.DASHBOARD, ERouteName.MANAGEMENT]);
+const selectedKeys = computed(() =>
+  [routes?.name?.toString() ?? ERouteName.DASHBOARD],
+);
+
+// watch(selectedKeys, (val) => {
+//   console.log(val);
+//   console.log('route', routes.name);
+// });
 </script>
