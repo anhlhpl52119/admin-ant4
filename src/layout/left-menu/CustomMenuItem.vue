026dd8f9e7f208437ff9a626c494000744a6c292
Hung Anh <anhlhpl52119@gmail.com>
2023-08-28 20:44:21 +0700

    feat: add dynamic menu

---
+++ src/layout/left-menu/CustomMenuItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <AMenuItem :key="data.name" class="flex-center">
+    <PieChartOutlined />
+    <!-- <span>{{ data.meta.title }}</span> -->
+    <RouterLink :to="{ name: data.name }" class="ml-5">
+      {{ data.meta.title }}
+    </RouterLink>
+  </AMenuItem>
+</template>
+
+<script lang="ts" setup>
+import {
+  PieChartOutlined,
+} from '@ant-design/icons-vue';
+import type { CustomRoute } from '@/router/typing';
+
+const props = defineProps<{ data: CustomRoute }>();
+const { data } = toRefs(props);
+</script>
