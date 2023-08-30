367db9d6fd31c8df22f5fc06897b62d3b0f883f4
Hung Anh <anhlhpl52119@gmail.com>
2023-08-30 18:17:29 +0700

    chore: rebase

---
+++ src/components/common/HelloComp.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3 class="text-red font-mono ml-20 text-40">
+      Hello from component
+    </h3>
+    <div>
+      Hello child
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
