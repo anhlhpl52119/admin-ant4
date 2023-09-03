ba8d5eb158b635a2f773040469d41a9f9507daa9
Hung Anh <anhlhpl52119@gmail.com>
2023-09-03 12:02:25 +0700

    del

--- src/router/module/index.ts
+++
@@ -1,9 +0,0 @@
-import dashboard from './dashboard';
-import user from './user';
-import management from './management';
-import error from './error';
-import demo from './demoError';
-import driver from './driver';
-import branch from './branch';
-
-export default [...driver, ...branch, ...dashboard, ...user, ...management, ...error, ...demo];
