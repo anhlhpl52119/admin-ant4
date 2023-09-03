03a872597d42b1332401561f114c6436b1c87606
Hung Anh <anhlhpl52119@gmail.com>
2023-09-03 12:02:36 +0700

    feat: refactor router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,24 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import type { App } from 'vue';
 import type { CustomRoute } from './typing';
 import { afterEach, beforeEach } from './hooks';
-import { dynamicRouterGenerator } from './router-factory';
-import MainLayout from '@/layout/index.vue';
-import { ERouteName } from '@/enums/router.enum';
-import { ERole } from '@/enums/common.enum';
-import { DEFAULT_ROUTE_PATH } from '@/constants/common.constant';
+import { whiteListRoutes } from './module/white-list/white-list';
 
-export const routes: CustomRoute[] = [{
-  path: '/',
-  name: ERouteName.MAIN_LAYOUT,
-  component: MainLayout,
-  redirect: DEFAULT_ROUTE_PATH,
-  meta: {
-    hiddenInMenu: false,
-    title: 'Home',
-    permit: [],
-  },
-  children: [],
-}];
+export const routes: CustomRoute[] = whiteListRoutes;
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,8 +12,6 @@ const router = createRouter({
 });
 
 export async function setupRouter(app: App) {
-  dynamicRouterGenerator(ERole.ADMIN);
-
   // Init router gruard
   beforeEach(router);
   afterEach(router);
