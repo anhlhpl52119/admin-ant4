ad98187cf8ba232a019647a377a169f57150d966
Hung Anh <anhlhpl52119@gmail.com>
2023-09-05 20:40:37 +0700

    feat: add new relationship query

--- src/apis/core/branch/model.d.ts
+++ src/apis/core/branch/model.d.ts
@@ -1,44 +1,45 @@
-export { }
+export {};
 declare global {
-    type Branch = {
-        id: string;
-        name: string;
-        code: string;
-        contact_number: string;
-        description: string;
-        email: string;
-        address: string;
-        status: string;
-        source: string;
-        source_id: string;
-        source_created_at: string;
-        source_updated_at: string;
-    }
-    
-    type SearchBranchResponse = {
-        branchs: Branch[]
-    }
+  type Branch = {
+    id: string;
+    name: string;
+    code: string;
+    contact_number: string;
+    description: string;
+    email: string;
+    address: string;
+    status: string;
+    source: string;
+    source_id: string;
+    source_created_at: string;
+    source_updated_at: string;
+  };
 
-    type SearchBranchQueryParams = ApiQuery<Branch>
+  type SearchBranchResponse = {
+    branches: Branch[];
+  };
 
-    type GetBranchDetailResponse = Branch
+  type BranchRelationShip = "retailer";
 
-    type CreateBranchRequestBody = {
-        name: string;
-        code: string;
-        description: string;
-        contact_number: string;
-        email: string;
-        address: string;
-    }
-    
-    type UpdateBranchRequestBody = {
-        name: string;
-        code: string;
-        description: string;
-        contact_number: string;
-        email: string;
-        address: string;
-    }
+  type SearchBranchQueryParams = ApiQuery<Branch, BranchRelationShip>;
 
+  type GetBranchDetailResponse = Branch;
+
+  type CreateBranchRequestBody = {
+    name: string;
+    code: string;
+    description: string;
+    contact_number: string;
+    email: string;
+    address: string;
+  };
+
+  type UpdateBranchRequestBody = {
+    name: string;
+    code: string;
+    description: string;
+    contact_number: string;
+    email: string;
+    address: string;
+  };
 }
