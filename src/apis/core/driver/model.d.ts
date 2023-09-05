e790d25b15eadd6594ca957c11b24852340eb795
Hung Anh <anhlhpl52119@gmail.com>
2023-09-05 20:40:41 +0700

    feat: add new relationship query

--- src/apis/core/driver/model.d.ts
+++ src/apis/core/driver/model.d.ts
@@ -1,56 +1,57 @@
-export {}
+export {};
 declare global {
-    type Driver = {
-        id: string;
-        name: string;
-        code: string;
-        description: string;
-        phone: string;
-        email: string;
-        address: string;
-        status: string;
-        source: string;
-        source_id: string;
-        source_created_at: string;
-        source_updated_at: string;
-    }
-    type SearchDriverResponse = {
-        drivers: Driver[]
-    }
+  type Driver = {
+    id: string;
+    name: string;
+    code: string;
+    description: string;
+    phone: string;
+    email: string;
+    address: string;
+    status: string;
+    source: string;
+    source_id: string;
+    source_created_at: string;
+    source_updated_at: string;
+  };
 
-    type SearchDriverQueryParams = ApiQuery<Driver>
-    
-    type GetDriverDetailResponse = {
-        id: string;
-        name: string;
-        code: string;
-        description: string;
-        phone: string;
-        email: string;
-        address: string;
-        status: string;
-        source: string;
-        source_id: string;
-        source_created_at: string;
-        source_updated_at: string;
-    }
-    
-    type CreateDriverRequestBody = {
-        name: string;
-        code: string;
-        description: string;
-        phone: string;
-        email: string;
-        address: string;
-    }
-    
-    type UpdateDriverRequestBody = {
-        name: string;
-        code: string;
-        description: string;
-        phone: string;
-        email: string;
-        address: string;
-    }
-    
+  type SearchDriverResponse = {
+    drivers: Driver[];
+  };
+  type DriverRelationShip = "group_drivers" | "retailer";
+  
+  type SearchDriverQueryParams = ApiQuery<Driver, DriverRelationShip>;
+
+  type GetDriverDetailResponse = {
+    id: string;
+    name: string;
+    code: string;
+    description: string;
+    phone: string;
+    email: string;
+    address: string;
+    status: string;
+    source: string;
+    source_id: string;
+    source_created_at: string;
+    source_updated_at: string;
+  };
+
+  type CreateDriverRequestBody = {
+    name: string;
+    code: string;
+    description: string;
+    phone: string;
+    email: string;
+    address: string;
+  };
+
+  type UpdateDriverRequestBody = {
+    name: string;
+    code: string;
+    description: string;
+    phone: string;
+    email: string;
+    address: string;
+  };
 }
