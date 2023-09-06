6b7b52831e116e37cae81e79651b125b5a0a5481
Hung Anh <anhlhpl52119@gmail.com>
2023-09-06 22:17:25 +0700

    chore: update model

--- src/apis/core/group-driver/model.d.ts
+++ src/apis/core/group-driver/model.d.ts
@@ -12,6 +12,8 @@ declare global {
     source_id: string;
     source_created_at: string;
     source_updated_at: string;
+    retailer?: any //TODO: add retailer
+    drivers?: Driver[]
   };
 
   type SearchGroupDriverResponse = {
