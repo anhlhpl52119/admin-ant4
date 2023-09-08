384356d6efd5efa59481d66208eba426bbb154e2
Hung Anh <anhlhpl52119@gmail.com>
2023-09-08 23:29:25 +0700

    style: reformat

--- src/enums/router.enum.ts
+++ src/enums/router.enum.ts
@@ -1,18 +1,43 @@
 export enum ERouteName {
-  DEMO = 'demo',
-  DRIVER = 'driver',
-  DRIVER_MANAGEMENT = 'driver-management',
+  /** Auth Routes */
+  LOGIN = 'login',
+
+  /** User Routes */
+  USER = 'user',
+  USER_PAGE1 = 'user-page1',
+  USER_PAGE2 = 'user-page2',
+  USER_PAGE3 = 'user-page3',
+  USER_PAGE4 = 'user-page4',
+  USER_PAGE5 = 'user-page5',
+  USER_PAGE6 = 'user-page6',
+
+  /** layout */
+  MAIN_LAYOUT = 'layout',
+
+  /** Branch Routes */
   BRANCH = 'branch',
   BRANCH_MANAGEMENT = 'branch-management',
+
+  /** Driver Routes */
+  DRIVER = 'driver',
   DRIVER_GROUP = 'driver-group',
+  DRIVER_MANAGEMENT = 'driver-management',
+
+  /** White List Routes */
+  WHITE_LIST = 'white-list',
+
+  /** Demo Routes */
+  DEMO = 'demo',
   DEMO_PAGE1 = 'demo-page1',
   DEMO_ERROR = 'DEMO-ERROR',
   DEMO_404 = 'DEMO-404',
-  MAIN_LAYOUT = 'layout',
+
+  /** Error Routes */
   ERROR = 'error',
   ERROR_404 = 'error-404',
   ERROR_500 = 'error-500',
   ERROR_403 = 'error-403',
+
   DASHBOARD = 'dashboard',
   DASHBOARD_PAGE1 = 'dashboard-page1',
   DASHBOARD_PAGE2 = 'dashboard-page2',
@@ -25,13 +50,5 @@ export enum ERouteName {
   MANAGEMENT_PAGE4 = 'management-page4',
   MANAGEMENT_PAGE5 = 'management-page5',
   MANAGEMENT_PAGE6 = 'management-page6',
-  USER = 'user',
-  USER_PAGE1 = 'user-page1',
-  USER_PAGE2 = 'user-page2',
-  USER_PAGE3 = 'user-page3',
-  USER_PAGE4 = 'user-page4',
-  USER_PAGE5 = 'user-page5',
-  USER_PAGE6 = 'user-page6',
-  LOGIN = 'login',
-  WHITE_LIST = 'white-list',
+
 }
