bf88e45928157964dbe42a8da3225754850b778e
Hung Anh <anhlhpl52119@gmail.com>
2023-09-08 23:29:14 +0700

    feat: add spec api id

--- src/enums/request.enum.ts
+++ src/enums/request.enum.ts
@@ -2,10 +2,14 @@ export enum ERequestMethod {
   PUT = 'PUT',
   GET = 'GET',
   POST = 'POST',
-  DEL = 'DELETE',
+  DELETE = 'DELETE',
   PATCH = 'PATCH',
 }
 export enum EStatusCode {
   UNAUTHORIZED = 401,
   UNKNOWN = 999,
 }
+export enum EApiId {
+  API1,
+  API2,
+};
