5e59021d113c09f68a1dae771fcaf5b8a675f581
Hung Anh <anhlhpl52119@gmail.com>
2023-09-09 22:25:24 +0700

    chore: update

--- src/plugins/asset-setup.ts
+++ src/plugins/asset-setup.ts
@@ -2,11 +2,11 @@
 import '@/assets/styles/transition.scss';
 
 // base
-import '@/assets/styles/normalize.scss';
 import '@/assets/styles/fonts.scss';
 import '@/assets/styles/root.scss';
 import '@/assets/styles/scroll-bar.scss';
 import '@/assets/styles/varr.scss';
+import '@/assets/styles/normalize.scss';
 
 // uno css
 import 'uno.css';
