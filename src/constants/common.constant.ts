15c66fae49ffd2b6e7cb49102aaf096a86caa9e5
Hung Anh <anhlhpl52119@gmail.com>
2023-09-09 22:24:49 +0700

    chore: add new varr

--- src/constants/common.constant.ts
+++ src/constants/common.constant.ts
@@ -1,1 +1,22 @@
+import type { SelectProps } from 'ant-design-vue';
+
 export const DEFAULT_ROUTE_PATH = '/dashboard/page1';
+
+export const VIEW_BY_OPTIONS: SelectProps['options'] = [
+  {
+    value: 10,
+    label: '10 / Trang',
+  },
+  {
+    value: 20,
+    label: '20 / Trang',
+  },
+  {
+    value: 30,
+    label: '30 / Trang',
+  },
+  {
+    value: 40,
+    label: '40 / Trang',
+  },
+];
