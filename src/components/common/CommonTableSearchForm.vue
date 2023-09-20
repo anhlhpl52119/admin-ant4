28a19ae61eee828d9adaee01effdcd59d6f17662
Hung Anh <anhlhpl52119@gmail.com>
2023-09-20 22:04:50 +0700

    chore: update search form

--- src/components/common/CommonTableSearchForm.vue
+++ src/components/common/CommonTableSearchForm.vue
@@ -1,63 +1,69 @@
 <template>
-  <section class="card flex items-end justify-between" @keypress.enter="onSearch">
-    <CInput
-      v-model:value="searchNameVal"
-      placeholder="Nhập tên để tìm kiếm..."
-      size="large"
-      :maxlength="100"
-      allow-clear
-      class="w-400 self-end"
-    >
-      <template #suffix>
-        <SearchOutlined />
-      </template>
-    </CInput>
-    <TransitionGroup name="fade" tag="ul" class="flex gap-10 flex-wrap">
-      <template v-for="item in state" :key="item.key">
-        <li class="search-form-items">
-          <CInput v-model:value="item.value" :label="item.label" size="small" class="rounded-full" />
-          <i class="i-ic:baseline-cancel" @click="remove(item.key)" />
+  <section class="card grid gap-10" @keypress.enter="onSearch">
+    <div class="flex items-center justify-between">
+      <CInput
+        v-model:value="searchNameVal"
+        placeholder="Nhập tên để tìm kiếm..."
+        size="large"
+        :maxlength="100"
+        allow-clear
+        class="w-400 self-end"
+      >
+        <template #suffix>
+          <SearchOutlined />
+        </template>
+      </CInput>
+      <ul class="justify-self-end flex gap-5 items-center">
+        <li v-if="state.length !== 0">
+          <AButton
+            size="small"
+            shape="round"
+            danger
+            :loading="loading"
+            class="group"
+            @click="clearAll"
+          >
+            <span>Xóa bộ lọc</span>
+            <template #icon>
+              <ClearOutlined />
+            </template>
+          </AButton>
         </li>
-      </template>
-      <AButton shape="round" size="small" class="self-end" @click="addNew">
-        + Thêm bộ lọc
-      </AButton>
-    </TransitionGroup>
-    <ul class="justify-self-end flex gap-5 items-center">
-      <li v-if="state.length !== 0">
-        <AButton
-          size="small"
-          shape="round"
-          danger
-          :loading="loading"
-          class="group"
-          @click="clearAll"
-        >
-          <span>Xóa bộ lọc</span>
-          <template #icon>
-            <ClearOutlined />
-          </template>
-        </AButton>
-      </li>
-      <li>
-        <AButton
-          type="primary" size="large"
-          :loading="loading"
-          @click="onSearch"
-        >
-          Tìm
-          <template #icon>
-            <SearchOutlined />
-          </template>
-        </AButton>
-      </li>
-    </ul>
+        <li>
+          <AButton shape="round" size="small" class="self-end" @click="addNew">
+            + Thêm bộ lọc
+          </AButton>
+        </li>
+        <li>
+          <AButton
+            type="primary" size="large"
+            :loading="loading"
+            @click="onSearch"
+          >
+            Tìm
+            <template #icon>
+              <SearchOutlined />
+            </template>
+          </AButton>
+        </li>
+      </ul>
+    </div>
+    <div v-if="state.length !== 0" ref="scrollElement" class="max-w-full overflow-x-auto overflow-y-hidden">
+      <TransitionGroup name="fade" tag="ul" class="flex gap-10 my-7">
+        <template v-for="item in state" :key="item.key">
+          <li class="search-form-items shrink-0 basis-170">
+            <CInput v-model:value="item.value" :label="item.label" size="small" class="rounded-full" />
+            <i class="i-ic:baseline-cancel" @click="remove(item.key)" />
+          </li>
+        </template>
+      </TransitionGroup>
+    </div>
   </section>
 </template>
 
 <script lang="ts" setup generic="T">
 import { ClearOutlined, SearchOutlined } from '@ant-design/icons-vue';
-import { randomPick } from '@/utils/common.util';
+import { randomPick, sleepFor } from '@/utils/common.util';
 
 const props = defineProps<{
   loading: boolean
@@ -69,6 +75,8 @@ const emits = defineEmits<{
   search: [v: ApiAttributeQuery<T>]
   reset: [v: void]
 }>();
+
+const scrollElement = ref<HTMLElement>();
 
 const { loading } = toRefs(props);
 interface TestVl {
@@ -108,9 +116,20 @@ const data: TestVl[] = [
 ];
 const state = ref<TestVl[]>([]);
 
-const addNew = () => {
+const addNew = async () => {
   const data2 = randomPick(data);
   state.value.push(data2);
+
+  await sleepFor(100);
+  if (!scrollElement.value) {
+    return;
+  }
+
+  scrollElement.value.scrollTo({
+    behavior: 'smooth',
+    top: 0,
+    left: 20000,
+  });
 };
 
 const remove = (key: string) => {
@@ -121,6 +140,7 @@ const onSearch = () => {
   searchNameVal.value = searchNameVal.value.replace(/ {2,}/g, ' '); // keep 1 space on search conditions
   emits('search', {});
 };
+
 const clearAll = () => {
   state.value = [];
   emits('reset');
