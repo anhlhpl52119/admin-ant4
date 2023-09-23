9489008a4242ea85803452fbdd52459bb32ebd2f
Hung Anh <anhlhpl52119@gmail.com>
2023-09-23 09:47:43 +0700

    refactor: cleanup

--- src/composable/useCommonTableMethod.ts
+++ src/composable/useCommonTableMethod.ts
@@ -7,6 +7,7 @@ interface Res<T> {
   total_page: number
   total_records: number
 }
+
 export const useCommonTableMethod = <T>(
   loadingNameSpace: EApiId,
   api: (optional?: ApiQuery<T, any>) => Promise<Res<T>>) => {
@@ -23,6 +24,49 @@ export const useCommonTableMethod = <T>(
     totalPage: 1,
     currentPage: 1,
     viewBy: 10,
+  });
+
+  const paginationSS = reactive({
+    page: 1,
+    items: 10,
+  });
+
+  const queriesFinal = ref<ApiAttributeQuery<T>>({}) as Ref<ApiAttributeQuery<T>>;
+
+  const fetchPr = computed<ApiQuery<T>>(() => ({
+    ...paginationSS,
+    query: { ...queriesFinal.value },
+  }));
+
+  const fetchPr2 = computed(() => ({
+    paging: { ...paginationSS },
+    queries: { ...queriesState.value },
+  }));
+  interface Page {
+    page: number
+    items: number
+  }
+  const fetch2 = async (forceOptions?: ApiQuery<T>) => {
+    const page: Page = { ...fetchPr2.value.paging };
+
+    stateRecords.value = [];
+
+    const params = { ...fetchPr.value, ...forceOptions };
+
+    const resApi = await api(params);
+
+    paginationState.viewBy = params?.items ?? 10;
+
+    stateRecords.value = resApi.records;
+    paginationState.totalRecord = resApi.total_records;
+    paginationState.totalPage = resApi.total_page;
+
+    if (forceOptions?.query) {
+      queriesState.value = { ...forceOptions.query };
+    }
+  };
+  watch(paginationSS, () => {
+    fetch2();
   });
 
   const fetchQueriesParams = computed<ApiQuery<T>>(() => ({
@@ -49,7 +93,7 @@ export const useCommonTableMethod = <T>(
   };
 
   const search = (queries?: ApiAttributeQuery<T>) => {
-    fetch({ query: queries });
+    fetch({ page: 1, query: queries });
   };
 
   const reload = () => {
