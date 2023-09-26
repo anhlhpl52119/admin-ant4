8580535b057467a1db4cf0f0dc4cb13b71e6a3aa
Hung Anh <anhlhpl52119@gmail.com>
2023-09-26 23:23:56 +0700

    refactor: table search queries

--- src/types/core.d.ts
+++ src/types/core.d.ts
@@ -10,4 +10,14 @@ declare global {
   type DeepRequired<T> = RequiredDeep<T>;
 
   type WidenLiteral<T> = T extends string | number | boolean ? ReturnType<T['valueOf']> : T;
+
+  type RansackQuery<T> = {
+    [K in keyof T as `${string & K}${T[K] extends string | null | boolean
+      ? "_eq" | "_cont"
+      : T[K] extends Date | null
+      ? "_lteq" | "_gteq" | "_gt" | "_lt"
+      : T[K] extends boolean | null
+      ? "_true" | "_false"
+      : never}`]: T[K];
+  };
 }
