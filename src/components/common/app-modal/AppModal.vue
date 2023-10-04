ff2bf35b5263c110152ad86180063c82972d6a88
Hung Anh <anhlhpl52119@gmail.com>
2023-10-04 22:59:10 +0700

    feat: init

---
+++ src/components/common/app-modal/AppModal.vue
@@ -0,0 +1,14 @@
+<template>
+  <AModal
+    v-model:open="openModal"
+    destroy-on-close
+    title="Basic Modal"
+    @ok="handleOk"
+  >
+    <Component :is="comp" v-bind="bindTest" v-on="vOnTest" />
+  </AModal>
+</template>
+
+<script lang="ts" setup>
+const handleOk = () => {};
+</script>
