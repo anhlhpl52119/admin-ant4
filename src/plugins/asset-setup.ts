41a926c9dc9d14d3fff8c7a2068e9223c753f34f
Hung Anh <anhlhpl52119@gmail.com>
2023-10-06 21:58:24 +0700

    refactor: add comment code

--- src/plugins/asset-setup.ts
+++ src/plugins/asset-setup.ts
@@ -1,11 +1,20 @@
 // vue transition
 import '@/assets/styles/transition.scss';
 
-// base
+// root base
 import '@/assets/styles/fonts.scss';
 import '@/assets/styles/root.scss';
+
+// customize scroll bar
 import '@/assets/styles/scroll-bar.scss';
+
+// common
 import '@/assets/styles/varr.scss';
+
+// antd overidden
+import '@/assets/styles/antd-overide.scss';
+
+// preflight css
 import '@/assets/styles/normalize.scss';
 
 // uno css
