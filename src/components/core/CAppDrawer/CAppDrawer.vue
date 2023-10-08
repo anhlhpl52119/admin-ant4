5e324ecee334f839d21a21034a422432b3a7acff
Hung Anh <anhlhpl52119@gmail.com>
2023-10-08 12:19:48 +0700

    feat: init

---
+++ src/components/core/CAppDrawer/CAppDrawer.vue
@@ -0,0 +1,19 @@
+<template>
+  <ADrawer
+    v-model:open="visible"
+    message="toto"
+    size="large"
+    title="Details"
+    placement="right"
+    destroyOnClose
+  >
+    <div class="p-20">
+      <!-- <Component :is="drawerComponent" /> -->
+    </div>
+  </ADrawer>
+</template>
+
+<script lang="ts" setup>
+// import { useApplicationStore } from '@/stores/application.store';
+const visible = ref(false);
+</script>
---
+++ src/components/core/CAppModal/CAppModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <template v-for="(value, id) in state" :key="id">
+    <AModal
+      :open="value.isOpen"
+      destroyOnClose
+      centered
+      :maskClosable="value.maskCloseable ?? false"
+      :title="value.headerTitle ?? ''"
+      @cancel="coreModal.close(id.toString())"
+    >
+      <Component
+        :is="value.component"
+        v-bind="value?.props ?? {}"
+        v-on="value?.event ?? {}"
+      />
+      <template #footer>
+        <div class="hidden" />
+      </template>
+    </AModal>
+  </template>
+</template>
+
+<script lang="ts" setup>
+const state = computed(() => Object.fromEntries(modalState));
+</script>
