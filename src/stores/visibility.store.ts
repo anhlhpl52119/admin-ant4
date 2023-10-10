350cd8edf6fd10900202e3de6234f92071cf224a
Hung Anh <anhlhpl52119@gmail.com>
2023-10-10 12:12:59 +0700

    feat: init

---
+++ src/stores/visibility.store.ts
@@ -0,0 +1,27 @@
+import type { EApiId } from '@/enums/request.enum';
+
+export const useVisibilityStore = defineStore('application-store', () => {
+  const loadingIdsState = reactive<Set<EApiId>>(new Set());
+  // state
+  const loadingIds = readonly(loadingIdsState);
+
+  // getter
+  const isAppLoading = computed(() => !!loadingIdsState.size);
+
+  // actions
+  const setLoadingId = (id: EApiId) => {
+    loadingIdsState.add(id);
+  };
+
+  const removeLoadingId = (id?: EApiId) => {
+    id && loadingIdsState.delete(id);
+  };
+
+  return {
+    loadingIds,
+    isAppLoading,
+
+    setLoadingId,
+    removeLoadingId,
+  };
+});
