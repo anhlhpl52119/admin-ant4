4d1d6d753b318bed8954cde6b91d13c1c698a01d
Hung Anh <anhlhpl52119@gmail.com>
2023-10-11 23:11:24 +0700

    feat: init

---
+++ src/stores/common.store.ts
@@ -0,0 +1,22 @@
+import { retailerApis } from '@/apis/core/retailer/retailer.api';
+
+export const useCommonStore = defineStore('common-store', () => {
+  const retailerTypesState = ref<API.RetailerType[]>([]);
+
+  const getRetailerTypes = async (forceFetch?: boolean) => {
+    if (retailerTypesState.value.length > 0 && !forceFetch) {
+      return retailerTypesState.value;
+    }
+    const rs = await retailerApis.getTypes();
+    if (!(rs && rs.data) || rs.data.length === 0) {
+      return [];
+    }
+    retailerTypesState.value = rs.data;
+
+    return rs.data;
+  };
+
+  return {
+    getRetailerTypes,
+  };
+});
