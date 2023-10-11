a57507650e0311a7a93c9255b74fab02a7081a05
Hung Anh <anhlhpl52119@gmail.com>
2023-10-11 23:12:01 +0700

    feat: global loading indicator

--- src/plugins/ant-design-setup.ts
+++ src/plugins/ant-design-setup.ts
@@ -1,9 +1,13 @@
 import type { App } from 'vue';
-import Antd from 'ant-design-vue';
+import Antd, { Spin } from 'ant-design-vue';
+import { h } from 'vue';
 
 // import 'ant-design-vue/dist/reset.css';
 
 // setup
 export const setupAntdv = (app: App) => {
+  // Spin.setDefaultIndicator({
+  //   indicator: h('i', { class: 'i-line-md:downloading-loop text-20' }),
+  // });
   app.use(Antd);
 };
