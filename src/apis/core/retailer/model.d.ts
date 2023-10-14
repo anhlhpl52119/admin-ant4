cda116860ffe58ebb99b745c7f5735d42eb350dd
Hung Anh <anhlhpl52119@gmail.com>
2023-10-15 00:02:17 +0700

    chore: update

--- src/apis/core/retailer/model.d.ts
+++ src/apis/core/retailer/model.d.ts
@@ -14,19 +14,29 @@ declare namespace API {
     updated_at?: Date;
     subdomain_api?: string;
     subdomain_fe?: string;
-    sync_status: RetailerSyncStatus
+    sync_status: RetailerSyncStatus;
     // relationship
     group_drivers?: GroupDriver[];
     drivers?: Driver[];
+    user?: any; // TODO
   };
 
   type SearchRetailerResponse = {
     retailers: Retailer[];
   };
 
-  type RetailerSyncStatus = 'not_config' | 'ready_to_sync' | 'synced' | 'sync_failed' | 'webhook_not_enough' | 'full_synced'
+  type RetailerSyncStatus =
+    | "not_config"
+    | "ready_to_sync"
+    | "synced"
+    | "sync_failed"
+    | "webhook_not_enough"
+    | "full_synced";
 
-  type RetailerRelationship = 'group_drivers' | 'drivers';
+  type RetailerRelationship = 
+  | "group_drivers" 
+  | "drivers" 
+  | "user";
 
   type SearchRetailerQueryParams = ApiCoreQuery<Retailer, RetailerRelationship>;
 
@@ -52,7 +62,29 @@ declare namespace API {
     description: string;
     created_at: string;
     updated_at: string;
-  }
+  };
 
-  type GetRetailerTypesResponse = ApiArrayResponse<RetailerType>
+  type GetRetailerTypesResponse = ApiArrayResponse<RetailerType>;
+
+
+  type RetailerConfig = {
+    id: string;
+    name: string;
+    code: string;
+    value: string;
+    description: string;
+    created_at?: Date;
+    updated_at?: Date;
+    retailer_id: string;
+  };
+
+  type GetRetailerConfigsResponse = ApiArrayResponse<RetailerConfig>;
+
+  type UpdateRetailerConfigsRequestBody = {
+    name: string;
+    code: string;
+    description: string;
+    value: string;
+    retailer_id: string;
+  };
 }
