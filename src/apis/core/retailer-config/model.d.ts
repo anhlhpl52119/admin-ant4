795f99a0658aa5b9162af376008fbf2a3dfc2c89
Hung Anh <anhlhpl52119@gmail.com>
2023-10-15 16:42:25 +0700

    chore: temp dim

--- src/apis/core/retailer-config/model.d.ts
+++ src/apis/core/retailer-config/model.d.ts
@@ -1,39 +1,30 @@
-declare namespace API {
-  type RetailerConfig = {
-    id: string;
-    name: string;
-    code: string;
-    value: string;
-    description: string;
-    created_at: Date;
-    updated_at: Date;
-    retailer_id: string;
-    retailer?: Retailer;
-  };
+// declare namespace API {
 
-  type SearchRetailerConfigResponse = {
-    retailer_configs: RetailerConfig[];
-  };
 
-  type RetailerConfigRelationship = 'retailer';
+//   type SearchRetailerConfigResponse = {
+//     retailer_configs: RetailerConfig[];
+//   };
 
-  type SearchRetailerConfigQueryParams = ApiCoreQuery<RetailerConfig, RetailerConfigRelationship>;
+//   type RetailerConfigRelationship = 'retailer';
 
-  type GetRetailerConfigDetailResponse = ApiObjectResponse<RetailerConfig>;
+//   type SearchRetailerConfigQueryParams = ApiCoreQuery<RetailerConfig, RetailerConfigRelationship>;
 
-  type CreateRetailerConfigRequestBody = {
-    name: string;
-    code: string;
-    description: string;
-    value: string;
-    retailer_id: string;
-  };
+//   // type GetRetailerConfigDetailResponse = ApiObjectResponse<RetailerConfig>;
 
-  type UpdateRetailerConfigRequestBody = {
-    name: string;
-    code: string;
-    description: string;
-    value: string;
-    retailer_id: string;
-  };
-}
+//   type CreateRetailerConfigRequestBody = {
+//     name: string;
+//     code: string;
+//     description: string;
+//     value: string;
+//     retailer_id: string;
+//   };
+
+//   type UpdateRetailerConfigRequestBody = {
+//     name: string;
+//     code: string;
+//     description: string;
+//     value: string;
+//     retailer_id: string;
+//   };
+
+// }
