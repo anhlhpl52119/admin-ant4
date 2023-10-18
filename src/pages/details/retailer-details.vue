ff7b746c93e93bdab58865d3ed142547f9f45ca9
Hung Anh <anhlhpl52119@gmail.com>
2023-10-18 23:59:56 +0700

    chore: update

--- src/pages/details/retailer-details.vue
+++ src/pages/details/retailer-details.vue
@@ -1,146 +1,183 @@
 <template>
-  <section class="card mt-0">
-    <div>
-      <ABadge status="success" dot class="text-20">
-        <AAvatar shape="square" size="large" :src="auto(50)" />
-      </ABadge>
-      <span class="text-spotlight text-20">{{ retailerState?.name || '-' }}</span>
-    </div>
-  </section>
-
-  <section class="card mt-20">
+  <div>
     <ASpin :spinning="!retailerState">
-      <ADescriptions title="Tổng quan" bordered>
-        <ADescriptionsItem label="Tên">
-          {{ retailerState?.name || '-' }}
-        </ADescriptionsItem>
-        <ADescriptionsItem label="Mô tả">
-          {{ retailerState?.description || '-' }}
-        </ADescriptionsItem>
-        <ADescriptionsItem label="Trạng thái ">
-          {{ retailerState?.status || '-' }}
-        </ADescriptionsItem>
-        <ADescriptionsItem label="Liên hệ">
-          {{ retailerState?.phone ?? '-' }}
-        </ADescriptionsItem>
-        <ADescriptionsItem label="Địa chỉ">
-          {{ retailerState?.address || '-' }}
-        </ADescriptionsItem>
-      </ADescriptions>
+      <section class="card mt-0 cursor-default">
+        <div class="flex-btw-center">
+          <div>
+            <ABadge status="success" dot class="text-20 ml-10">
+              <AAvatar shape="square" size="large" :src="randomAvatar(50)" />
+            </ABadge>
+
+            <span class="text-spotlight text-24 ml-7">{{ retailerState?.name || '-' }}</span>
+            <span class="text-desc text-12 ml-7">{{ retailerState?.code || '-' }}</span>
+          </div>
+
+          <AButton size="large" @click="openModel(retailerState?.id ?? '')">
+            Chỉnh sửa
+          </AButton>
+        </div>
+        <div class="mt-30">
+          <ul class="flex gap-30">
+            <li>
+              <div class="flex items-center gap-7">
+                <i class="i-solar:phone-calling-rounded-bold text-20 block text-primary" />
+                <span>Điện thoại</span>
+              </div>
+
+              <div class="mt-10" @click="copyText(retailerState?.phone ?? '')">
+                <span class="text-spotlight hover:underline cursor-pointer">{{ retailerState?.phone ?? '-' }}</span>
+              </div>
+            </li>
+            <ADivider type="vertical" class="h-50" />
+            <li>
+              <div class="flex items-center gap-7">
+                <i class="i-ic:round-alternate-email text-20 block text-primary" />
+                <span>Email</span>
+              </div>
+              <div class="mt-10" @click="copyText(retailerState?.email ?? '')">
+                <span class="text-spotlight hover:underline cursor-pointer">{{ retailerState?.email ?? '-' }}</span>
+              </div>
+            </li>
+            <ADivider type="vertical" class="h-50" />
+            <li>
+              <p>Số tài xế</p>
+              <div class="mt-10">
+                <span class="text-spotlight">{{ totalDrivers }}</span>
+              </div>
+            </li>
+            <ADivider type="vertical" class="h-50" />
+            <li>
+              <div class="flex items-center gap-7">
+                <i class="i-tdesign:system-sum text-20 block text-primary" />
+                <span>{{ retailerSourceName }}</span>
+              </div>
+              <div class="mt-10">
+                <ATag :color="syncStatusTag(retailerState?.sync_status ?? 'not_config').color">
+                  {{ syncStatusTag(retailerState?.sync_status ?? 'not_config').content.toUpperCase() }}
+                </ATag>
+              </div>
+            </li>
+            <ADivider type="vertical" class="h-50" />
+          </ul>
+        </div>
+      </section>
     </ASpin>
-    <ADivider>Cấu hình</ADivider>
-    <!-- Nhóm Tài xế -->
-    <ASpin :spinning="!retailerState">
-      <VerticalTabsContainer
-        v-model="selectedPlatform"
-        :items="kiotVietOption"
-        contentClass=""
-        class="mt-16"
-      >
-        <!-- header -->
-        <template #header>
-          <div class="p-16 text-18 font-medium">
-            Cấu hình
-          </div>
-        </template>
-        <!-- actions -->
-        <template #actions>
-          <div class="text-center p-12">
-            <AButton>+ Thêm mới</AButton>
-          </div>
-        </template>
-        <!-- content -->
-        <div class="">
-          <div class="b-b b-b-abd b-b-solid p-16">
-            <div class="flex justify-end gap-10">
-              <AButton
-                :loading="loadingIds.has(EApiId.RETAILER_CHECK_REQUIRE_CONFIG)"
-                @click="checkCommonConfig"
-              >
-                Test required config
-              </AButton>
 
-              <AButton type="primary" @click="onEdit">
-                Edit
-              </AButton>
+    <section class="card mt-20">
+      <ADivider>Cấu hình</ADivider>
+      <!-- Nhóm Tài xế -->
+      <ASpin :spinning="!retailerState">
+        <VerticalTabsContainer
+          v-model="selectedPlatform"
+          :items="kiotVietOption"
+          contentClass=""
+          class="mt-16"
+        >
+          <!-- header -->
+          <template #header>
+            <div class="p-16 text-18 font-medium">
+              Cấu hình
+            </div>
+          </template>
+          <!-- actions -->
+          <template #actions>
+            <div class="text-center p-12">
+              <AButton>+ Thêm mới</AButton>
+            </div>
+          </template>
+          <!-- content -->
+          <div class="">
+            <div class="b-b b-b-abd b-b-solid p-16">
+              <div class="flex justify-end gap-10">
+                <AButton
+                  :loading="loadingIds.has(EApiId.RETAILER_CHECK_REQUIRE_CONFIG)"
+                  @click="checkCommonConfig"
+                >
+                  Test required config
+                </AButton>
+
+                <AButton type="primary" @click="onEdit">
+                  Edit
+                </AButton>
+              </div>
+            </div>
+            <div class="p-16">
+              <AList
+                :data-source="configsState"
+                class=""
+              >
+                <template #renderItem="{ item } : {item: API.RetailerConfig}">
+                  <AListItem :key="item.id">
+                    <template #actions>
+                      <span>{{ item?.updated_at || '-' }}</span>
+                    </template>
+                    <AListItemMeta>
+                      <template #title>
+                        {{ item.name }}
+                      </template>
+                      <template #description>
+                        {{ item?.value || '-' }}
+                      </template>
+                    </AListItemMeta>
+                  </AListItem>
+                </template>
+              </AList>
             </div>
           </div>
+        </VerticalTabsContainer>
+      </ASpin>
+      <ADivider>Nhóm Tài xế</ADivider>
+      <!-- Nhóm Tài xế -->
+      <ASpin :spinning="!retailerState">
+        <VerticalTabsContainer
+          v-model="selectedValue"
+          :items="tabItems"
+          contentClass="max-h-350 overflow-y-scroll"
+          tabClass="max-h-350 overflow-y-scroll"
+          class="mt-16"
+        >
+          <!-- header -->
+          <template #header>
+            <div class="p-16 text-18 font-medium">
+              Nhóm tài xế
+            </div>
+          </template>
+          <!-- actions -->
+          <template #actions>
+            <div class="text-center p-12">
+              <AButton>+ Thêm mới</AButton>
+            </div>
+          </template>
+          <!-- content -->
           <div class="p-16">
             <AList
-              :data-source="configsState"
+              :data-source="driversState"
               class=""
             >
-              <template #renderItem="{ item } : {item: API.RetailerConfig}">
+              <template #renderItem="{ item, index } : {item: API.Driver, index: number}">
                 <AListItem :key="item.id">
                   <template #actions>
-                    <span>{{ item?.updated_at || '-' }}</span>
+                    <a>View Profile</a>
                   </template>
                   <AListItemMeta>
                     <template #title>
                       {{ item.name }}
                     </template>
+                    <template #avatar>
+                      <AAvatar size="large" :src="randomAvatar(index)" />
+                    </template>
                     <template #description>
-                      {{ item?.value || '-' }}
+                      {{ item.description }}
                     </template>
                   </AListItemMeta>
                 </AListItem>
               </template>
             </AList>
           </div>
-        </div>
-      </VerticalTabsContainer>
-    </ASpin>
-    <ADivider>Nhóm Tài xế</ADivider>
-    <!-- Nhóm Tài xế -->
-    <ASpin :spinning="!retailerState">
-      <VerticalTabsContainer
-        v-model="selectedValue"
-        :items="tabItems"
-        contentClass="max-h-350 overflow-y-scroll"
-        tabClass="max-h-350 overflow-y-scroll"
-        class="mt-16"
-      >
-        <!-- header -->
-        <template #header>
-          <div class="p-16 text-18 font-medium">
-            Nhóm tài xế
-          </div>
-        </template>
-        <!-- actions -->
-        <template #actions>
-          <div class="text-center p-12">
-            <AButton>+ Thêm mới</AButton>
-          </div>
-        </template>
-        <!-- content -->
-        <div class="p-16">
-          <AList
-            :data-source="driversState"
-            class=""
-          >
-            <template #renderItem="{ item, index } : {item: API.Driver, index: number}">
-              <AListItem :key="item.id">
-                <template #actions>
-                  <a>View Profile</a>
-                </template>
-                <AListItemMeta>
-                  <template #title>
-                    {{ item.name }}
-                  </template>
-                  <template #avatar>
-                    <AAvatar size="large" :src="auto(index)" />
-                  </template>
-                  <template #description>
-                    {{ item.description }}
-                  </template>
-                </AListItemMeta>
-              </AListItem>
-            </template>
-          </AList>
-        </div>
-      </VerticalTabsContainer>
-    </ASpin>
-  </section>
+        </VerticalTabsContainer>
+      </ASpin>
+    </section>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -148,6 +185,7 @@ import { Modal, message } from 'ant-design-vue';
 import { retailerApis } from '@/apis/core/retailer/retailer.api';
 import { driverApis } from '@/apis/core/driver/driver.api';
 import { useVisibilityStore } from '@/stores/visibility.store';
+import { useCommonStore } from '@/stores/common.store';
 import { EApiId } from '@/enums/request.enum';
 
 // *this props also passthrough Router, checkout Retailer router module '/details'
@@ -156,18 +194,62 @@ const props = defineProps<{
 }>();
 
 const ConfigUpdateForm = defineAsyncComponent(() => import('@/components/form/RetailerConfigUpdateForm.vue'));
+const RetailerCreateUpdateForm = defineAsyncComponent(() => import('@/components/form/RetailerCreateUpdateForm.vue'));
+
 const { loadingIds } = storeToRefs(useVisibilityStore());
 const { retailerId } = toRefs(props);
+const { getRetailerTypes } = useCommonStore();
 
 const retailerState = ref<API.Retailer>();
 const driversState = ref<API.Driver[]>([]);
 const configsState = ref<API.RetailerConfig[]>([]);
+const retailerType = ref<API.RetailerType[]>([]);
 
 const groupDriver = computed(() => retailerState.value?.group_drivers ?? []);
 const userRetailer = computed(() => retailerState.value?.user ?? undefined);
 
-const auto = (idx: number) => {
+const totalDrivers = computed(() => driversState.value.length);
+
+const randomAvatar = (idx: number) => {
   return `https://i.pravatar.cc/150?img=${idx}`;
+};
+
+const copyText = async (val: string) => {
+  message.destroy();
+  try {
+    await navigator.clipboard.writeText(val);
+    message.success(`Copied: ${val}`);
+  }
+  catch (e) {}
+};
+
+const retailerSourceName = computed(() => {
+  console.log(retailerState.value?.code);
+  const source = retailerType.value.find(i => i.code === retailerState.value?.source);
+  if (!source) {
+    return 'Chưa có source';
+  }
+
+  return source.name;
+});
+
+const syncStatusTag = (status: API.RetailerSyncStatus) => {
+  switch (status) {
+    case 'not_config':
+      return { color: 'geekblue', content: 'Chưa config' };
+    case 'full_synced':
+      return { color: 'success', content: 'Full sync' };
+    case 'ready_to_sync':
+      return { color: 'cyan', content: 'Sẵn sàng sync' };
+    case 'sync_failed':
+      return { color: 'magenta', content: 'Sync thất bại' };
+    case 'synced':
+      return { color: 'success', content: 'Đã sync' };
+    case 'webhook_not_enough':
+      return { color: 'purple', content: 'Webhook không đủ' };
+    default:
+      return { color: 'purple', content: 'chưa xác định' };
+  }
 };
 
 const initConfig = async () => {
@@ -220,6 +302,7 @@ const checkCommonConfig = async () => {
   const content = missingItems.split(',').map(i => h('div', i));
   Modal.error({ title, content });
 };
+
 // dfdf________________________________________________________________________________________________
 
 const selectedValue = ref('');
@@ -237,6 +320,7 @@ const tabItems = computed(() => {
 const kiotVietOption = [{ label: 'KiotViệt', value: '1' }];
 const selectedPlatform = ref('1');// kiotViet
 const initDetails = async () => {
+  retailerType.value = await getRetailerTypes();
   if (!retailerId?.value) {
     message.error('Thiếu retailerId');
 
@@ -260,6 +344,25 @@ const initDriver = async () => {
     return;
   }
   driversState.value = res.data.drivers;
+};
+
+const handleSuccess = (modalId: string) => {
+  coreModal.close(modalId);
+};
+
+const openModel = (retailerId?: string) => {
+  const title = retailerId ? 'Cập nhật thông tin nhà bán lẻ' : 'Tạo mới nhà bán lẻ';
+  const modalId = coreModal.show({
+    component: RetailerCreateUpdateForm,
+    title,
+    props: {
+      retailerId: retailerId ?? '',
+    },
+    emits: {
+      success: () => handleSuccess(modalId),
+      cancel: () => coreModal.close(modalId),
+    },
+  });
 };
 
 // _______________________________________________________________________________________________
