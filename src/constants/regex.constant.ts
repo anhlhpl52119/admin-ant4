0bc8dca9e826812b81b8fad4db895e82077ae890
Hung Anh <anhlhpl52119@gmail.com>
2023-10-24 23:00:33 +0700

    feat: init

---
+++ src/constants/regex.constant.ts
@@ -0,0 +1,7 @@
+export const TEXT_ONLY = /[0-9]/g;
+export const NUMBER_ONLY = /[^0-9]/g;
+export const BEGIN_BY_SPACE = /^\s/;
+export const INCLUDE_SPACE = /\s/g;
+export const MULTIPLE_SPACE_ADJACENT = /\s+/g;
+export const VALID_EMAIL_FORMAT = '';
+export const MORE_THAN_ONE_EMAIL_SIGN = /^([^@]*@[^@]*)@/;
