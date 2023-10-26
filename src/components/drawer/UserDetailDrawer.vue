e9ab11f826c9b3a6b28e615972ce419a512cc6f1
Hung Anh <anhlhpl52119@gmail.com>
2023-10-26 20:55:48 +0700

    feat: init

---
+++ src/components/drawer/UserDetailDrawer.vue
@@ -0,0 +1,114 @@
+<template>
+  <ADrawer
+    :open="isOpen"
+    width="90rem"
+    :title="title"
+    placement="right"
+    destroyOnClose
+    @close="onClose"
+  >
+    <template #extra>
+      <div class="flex gap-16">
+        <AButton @click="openModel">
+          Edit
+        </AButton>
+        <AButton :icon="h(DeleteOutlined)" danger type="primary" @click="onDelete">
+          Xóa
+        </AButton>
+      </div>
+    </template>
+    <div v-if="!userDetail" class="text-center h-full">
+      <ASpin size="large" />
+    </div>
+    <template v-else>
+      <ADescriptions title="Thông tin người dùng" bordered size="small">
+        <ADescriptionsItem label="Tên">
+          {{ userDetail?.name || '' }}
+        </ADescriptionsItem>
+        <ADescriptionsItem label="Phân Quyền">
+          {{ userDetail?.role || '' }}
+        </ADescriptionsItem>
+        <ADescriptionsItem label="Email">
+          {{ userDetail?.email || '' }}
+        </ADescriptionsItem>
+      </ADescriptions>
+      <ADivider />
+    </template>
+  </ADrawer>
+</template>
+
+<script lang="ts" setup>
+import { DeleteOutlined } from '@ant-design/icons-vue';
+import { retailerUserApis } from '@/apis/retailer/user-mgt/user-mgt.api';
+
+const props = defineProps<{
+  isOpen: boolean
+  title: string
+  userId: string
+  item?: API.RetailerUser
+}>();
+
+const emit = defineEmits<{
+  close: [v: boolean]
+}>();
+
+const RetailerUserCreateUpdateForm = defineAsyncComponent(() => import('@/components/form/RetailerUserCreateUpdateForm.vue'));
+
+const { isOpen, userId } = toRefs(props);
+const userDetail = ref<API.RetailerUser>();
+
+const onClose = () => {
+  userDetail.value = undefined;
+  emit('close', false);
+};
+
+const initGroupDriver = async () => {
+  if (!userId.value) {
+    return;
+  }
+  const rs = await retailerUserApis.getDetails(userId.value, { includes: ['belonged_retailer'] });
+  if (!(rs && rs.data)) {
+    return;
+  }
+  userDetail.value = rs.data;
+};
+
+const handleSuccess = (modalId: string) => {
+  coreModal.close(modalId);
+  initGroupDriver();
+};
+
+const openModel = () => {
+  const modalId = coreModal.show({
+    component: RetailerUserCreateUpdateForm,
+    title: 'Cập nhật thông tin người dùng',
+    props: {
+      userId: userDetail.value?.id ?? '',
+    },
+    emits: {
+      success: () => handleSuccess(modalId),
+      cancel: () => coreModal.close(modalId),
+    },
+  });
+};
+
+const onDelete = async () => {
+  const confirm = await showConfirmAlert({ content: 'Xóa toàn bộ dữ liệu của người dùng này?' });
+  if (!confirm) {
+    return;
+  }
+  if (!userDetail.value?.id) {
+    return;
+  }
+  const rs = await retailerUserApis.delete(userDetail.value.id);
+  if (rs && rs.data) {
+    emit('close', true);
+  }
+};
+
+watch(isOpen, (val) => {
+  if (val && !userDetail.value) {
+    initGroupDriver();
+  }
+});
+</script>
