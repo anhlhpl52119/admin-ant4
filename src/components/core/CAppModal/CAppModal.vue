754ca30bdab52c1f8521d85f900c505612219b7b
Hung Anh <anhlhpl52119@gmail.com>
2023-11-04 01:19:29 +0700

    feat: self close

--- src/components/core/CAppModal/CAppModal.vue
+++ src/components/core/CAppModal/CAppModal.vue
@@ -8,12 +8,13 @@
       :width="value.modalWidth"
       :maskClosable="value.maskCloseable ?? false"
       :title="value.headerTitle ?? ''"
-      @cancel="coreModal.close(id.toString())"
+      @cancel="closeModal(id.toString())"
     >
       <Component
         :is="value.component"
         v-bind="value?.props ?? {}"
         v-on="value?.event ?? {}"
+        @cancel="closeModal(id.toString())"
       />
     </AModal>
   </template>
@@ -21,4 +22,8 @@
 
 <script lang="ts" setup>
 const state = computed(() => Object.fromEntries(modalState));
+
+const closeModal = (id: string) => {
+  coreModal.close(id.toString());
+};
 </script>
