69daa9651b73b3a1ffdbf2b9aee64f8beebe6659
Hung Anh <anhlhpl52119@gmail.com>
2023-11-08 23:58:23 +0700

    feat: new badge

---
+++ src/components/field/FieldNewBadge.vue
@@ -0,0 +1,14 @@
+<template>
+  <slot />
+  <sup v-if="isNew" class="ml2 p-2 b-abd bg-success rounded-5 text-white text-8">New</sup>
+</template>
+
+<script lang="ts" setup>
+import { isFiveMinutesAgo } from '@/utils/date.util';
+
+const props = defineProps<{
+  createTime: OrNullish<string | number | Date>
+}>();
+
+const isNew = computed(() => isFiveMinutesAgo(props.createTime));
+</script>
