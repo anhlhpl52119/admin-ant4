8930e528c1537372ad0234e97335458d7666ff40
Hung Anh <anhlhpl52119@gmail.com>
2023-11-08 16:31:23 +0700

    feat: copyable field

---
+++ src/components/field/FieldTextCopyable.vue
@@ -0,0 +1,24 @@
+<template>
+  <span v-bind="$attrs" class="hover:underline cursor-pointer text-primary" @click="onCopy">{{ copyContent }}</span>
+</template>
+
+<script lang="ts" setup>
+import { message } from 'ant-design-vue';
+
+const props = defineProps<{
+  copyContent: string | number
+  showIcon?: boolean
+}>();
+
+const { copyContent } = toRefs(props);
+
+const onCopy = async () => {
+  try {
+    await navigator.clipboard.writeText(copyContent.value.toString());
+    message.success({ content: `Sao chép: ${copyContent.value}`, duration: 1.2, key: 'msgKey' });
+  }
+  catch (e) {
+    message.error({ content: 'Không thể sao chép', duration: 1.2, key: 'msgKey' });
+  }
+};
+</script>
--- src/components/overview/retailer/DriverOverview.vue
+++ src/components/overview/retailer/DriverOverview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-w-650">
+  <div>
     <ASpin size="large" :spinning="loadIdsHas(EApiId.DRIVER_SEARCH)">
       <ADescriptions
         :bordered="true"
@@ -22,51 +22,108 @@
           {{ formatDate(driverState?.updated_at) }}
         </ADescriptionsItem>
       </ADescriptions>
+      <div>
+        <pre>{{ driverInvoices.records }}</pre>
+        <pre>{{ driverTransaction.records }}</pre>
+      </div>
     </ASpin>
-    <!-- Extra button -->
-    <div v-if="!hideExtraBtn" class="flex flex-row-reverse mt-16">
-      <AButton @click="$emit('cancel')">
-        Đóng
-      </AButton>
-    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { retailerDriverApis } from '@/apis/retailer/driver-mgt/driver-mgt';
+import { invoiceApis } from '@/apis/retailer/source-invoice-mgt/source-invoice-mgt';
+import { transactionHistoryApis } from '@/apis/retailer/transaction-mgt/transaction-mgt';
 import { EApiId } from '@/enums/request.enum';
 import { formatDate } from '@/utils/date.util';
 
 const props = defineProps<{
   driverId: string
-  driver?: API.Driver
-  hideExtraBtn?: boolean
+  showRecentTransaction?: boolean
+  showRecentInvoices?: boolean
 }>();
 
-defineEmits<{
-  cancel: [v: void]
+const emits = defineEmits<{
+  cancel: [v: void] // use for close modal if this component use in AppModal
 }>();
 
-const { driverId, driver } = toRefs(props);
+const { driverId } = toRefs(props);
 
-const driverState = ref<OrNull<API.Driver>>(null);
+const driverState = ref<API.Driver>();
+const driverInvoices = reactive({
+  records: [] as API.SourceInvoice[],
+  total: 0,
+});
+
+const driverTransaction = reactive({
+  records: [] as API.TransactionHistory[],
+  total: 0,
+});
 
 const { loadIdsHas } = storeToRefs(useLoaderStore());
 
-const init = async () => {
-  if (driver?.value) {
-    driverState.value = driver.value;
+const getDriverLatestInvoices = async (driverId: string) => {
+  const rs = await invoiceApis.search({
+    query: {
+      driver_id_eq: driverId,
+      status_eq: 'active',
+      transaction_history_id_null: 'true',
+      s: 'invoice_date desc',
+    },
+    items: 10,
+  });
+  const invoices = rs?.data?.source_invoices;
+  if (!invoices || invoices.length === 0) {
     return;
   }
-  const rs = await retailerDriverApis.searchMyDrivers({ query: { id_eq: props.driverId }, includes: ['group_drivers'] });
-  if (!(rs && rs.data) || rs.data.drivers.length === 0) {
-    return;
-  }
-  [driverState.value] = rs.data.drivers;
+  driverInvoices.records = invoices;
+  driverInvoices.total = rs.data.total_records;
 };
 
-watchEffect(() => {
-  if (!driverId.value) { return; }
+const getDriverLatestTransaction = async (driverId: string) => {
+  const rs = await transactionHistoryApis.search({
+    query: {
+      driver_id_eq: driverId,
+      s: 'transaction_date desc',
+    },
+  });
+  const transactions = rs?.data?.transaction_histories;
+  if (!transactions || transactions.length === 0) {
+    return;
+  }
+  driverTransaction.records = transactions;
+  driverTransaction.total = rs.data.total_records;
+};
+const getDriverInfo = async (driverId: string): Promise<boolean> => {
+  const rs = await retailerDriverApis.searchMyDrivers({
+    query: {
+      id_eq: driverId,
+    },
+  });
+  if (!(rs && rs.data) || rs.data.drivers.length === 0) {
+    // server error or can't fetch driver Id
+    return false;
+  }
+  [driverState.value] = rs.data.drivers;
+  return true;
+};
+
+const init = async () => {
+  if (!await getDriverInfo(driverId.value)) {
+    emits('cancel');
+    await showAsyncAlert({ content: 'Không thể tải thông tin tài xế, xin hãy thử lại', severity: 'error' });
+    return;
+  }
+  props.showRecentInvoices && getDriverLatestInvoices(driverId.value);
+  props.showRecentTransaction && getDriverLatestTransaction(driverId.value);
+};
+
+watchEffect(async () => {
+  if (!driverId.value) {
+    emits('cancel');
+    await showAsyncAlert({ content: 'Không tìm thấy mã định danh của tài xế', severity: 'error' });
+    return;
+  }
   init();
 });
 </script>
