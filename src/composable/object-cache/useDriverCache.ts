7165be836787f0f591f501b5cbf686fb7d2c4c76
Hung Anh <anhlhpl52119@gmail.com>
2023-11-12 22:20:45 +0700

    feat: driver cache

--- src/composable/object-cache/useDriverCache.ts
+++ src/composable/object-cache/useDriverCache.ts
@@ -1,68 +1,182 @@
+import type { Dayjs } from 'dayjs';
 import { invoiceApis } from '@/apis/retailer/source-invoice-mgt/source-invoice-mgt';
 import { transactionHistoryApis } from '@/apis/retailer/transaction-mgt/transaction-mgt';
+import { formatDate, isValidDate } from '@/utils/date.util';
+import { EDateFormat } from '@/enums/common.enum';
+import type { ETransactionStatus } from '@/enums/api.enum';
 
-export const useDriverCache = (driverId: Ref<string>) => {
-  /**
-   * Get latest invoice of driver by driver id request params
-   * - order date descending
-   */
-  const getDriverUnpaidInvoiceParams: API.SearchSourceInvoiceQueryParams['query'] = {
-    driver_id_eq: driverId.value,
-    transaction_history_id_null: 'true',
-    s: 'invoice_date desc',
+type DriverInvoiceType = 'unpaid' | 'all';
+type DateOrder = 'desc' | 'asc'
+interface DriverInvoiceQuery {
+  driverId: string
+  invoiceType: DriverInvoiceType
+  page?: number
+  items?: number
+  dateOrder?: DateOrder
+  range?: [Dayjs, Dayjs]
+}
+
+interface DriverTransactionQuery {
+  driverId: string
+  transactionStatus: `${ETransactionStatus}`
+  page?: number
+  items?: number
+  dateOrder?: DateOrder
+  range?: [Dayjs, Dayjs]
+}
+
+interface DriverInvoicesResponse {
+  currentPage: number
+  totalRecord: number
+  totalPage: number
+  sourceInvoices: API.SourceInvoice[]
+}
+
+interface DriverTransactionResponse {
+  currentPage: number
+  totalRecord: number
+  totalPage: number
+  transactionHistories: API.TransactionHistory[]
+}
+
+const FALL_BACK_DRIVER_INVOICES_RESPONSE = {
+  sourceInvoices: [],
+  totalPage: 1,
+  currentPage: 1,
+  totalRecord: 0,
+};
+
+const FALL_BACK_DRIVER_TRANSACTION_RESPONSE = {
+  transactionHistories: [],
+  totalPage: 1,
+  currentPage: 1,
+  totalRecord: 0,
+};
+
+export const useDriverCache = () => {
+  const getDriverInvoices = async (props: DriverInvoiceQuery): Promise<DriverInvoicesResponse> => {
+    if (!props.driverId) {
+      return FALL_BACK_DRIVER_INVOICES_RESPONSE;
+    }
+
+    const query: API.SearchSourceInvoiceQueryParams['query'] = {
+      driver_id_eq: props.driverId,
+      s: `invoice_date ${props?.dateOrder || 'desc'}`,
+    };
+
+    const fromDate = props?.range?.[0] || undefined;
+    const toDate = props?.range?.[1] || undefined;
+
+    // query date order
+    if (isValidDate(fromDate) && isValidDate(toDate)) {
+      Object.assign(query, {
+        invoice_date_gteq: formatDate(fromDate, EDateFormat.DATE_API_QUERY),
+        invoice_date_lteq: formatDate(toDate, EDateFormat.DATE_API_QUERY),
+      });
+    }
+
+    const apiPayload: API.SearchSourceInvoiceQueryParams = {
+      items: props?.items || 1,
+      page: props?.page || 1,
+      query,
+    };
+    // fetch API
+    const rs = await invoiceApis.search(apiPayload);
+
+    // fallback case
+    if (!rs?.data?.source_invoices) {
+      return FALL_BACK_DRIVER_INVOICES_RESPONSE;
+    }
+
+    // success case
+    return {
+      sourceInvoices: rs?.data?.source_invoices || [],
+      totalPage: rs?.data?.total_page || 1,
+      currentPage: rs?.data?.current_page || 1,
+      totalRecord: rs?.data?.total_records || 0,
+    };
   };
 
-  /**
-   * Get latest transaction of pending status by driver id
-   * - order date descending
-   */
-  const getDriverTransactionIsPending: API.SearchTransactionQueryParams['query'] = {
-    driver_id_eq: driverId.value,
-    transaction_history_code_not_null: 'true',
-    status_eq: 'pending',
-    s: 'transaction_date desc',
-  };
-
-  const getTotalDriverInvoceCount = async (driverId: string): Promise<number> => {
+  const countDriverInvoices = async (driverId: string, type: DriverInvoiceType = 'all'): Promise<number> => {
+    const query: API.SearchSourceInvoiceQueryParams['query'] = {
+      driver_id_eq: driverId,
+      status_eq: 'active',
+    };
+    if (type === 'unpaid') {
+      Object.assign(query, { transaction_history_id_null: 'true' });
+    }
     const rs = await invoiceApis.search({
       items: 1,
-      page: 1,
-      query: {
-        driver_id_eq: driverId,
-      },
+      query,
     });
-    if (!rs?.data?.total_records) {
+    if (!(rs?.data?.total_records)) {
       return 0;
     }
-    return rs.data.total_records;
+    return rs?.data?.total_records;
   };
 
-  const fetchDriverLatestUnpaidInvoice = async (page: number = 1) => {
-    /**
-     * TODO: get total unpaid to manage "Create Transaction" button state
-     */
-    const rs = await invoiceApis.search({
-      items: 10,
-      page,
-      query: getDriverUnpaidInvoiceParams,
-    });
-
-    const invoices = rs?.data?.source_invoices;
-    if (!invoices || invoices.length === 0) { return; }
-    console.log('objsdect');
-    // driverInvoices.records = invoices;
-    // driverInvoices.total = rs.data.total_records;
-  };
-
-  const fetchDriverPendingTransaction = async (page: number = 1) => {
-    /**
-     * TODO: get total pending to visible badge on tab
-     * + get first record of "transaction_date" to show timeFromNow() to show last time paid
-     */
+  const countDriverTransactions = async (driverId: string, status?: `${ETransactionStatus}`): Promise<number> => {
     const rs = await transactionHistoryApis.search({
-      items: 10,
-      page,
-      query: getDriverTransactionIsPending,
+      items: 1,
+      query: {
+        driver_id_eq: driverId,
+        status_eq: status,
+      },
     });
+    if (!(rs?.data?.total_records)) {
+      return 0;
+    }
+    return rs?.data?.total_records;
+  };
+
+  const getDriverTransaction = async (props: DriverTransactionQuery): Promise<DriverTransactionResponse> => {
+    if (!props.driverId) {
+      return FALL_BACK_DRIVER_TRANSACTION_RESPONSE;
+    }
+
+    const query: API.SearchTransactionQueryParams['query'] = {
+      driver_id_eq: props.driverId,
+      s: `transaction_date ${props?.dateOrder || 'desc'}`,
+    };
+
+    const fromDate = props?.range?.[0] || undefined;
+    const toDate = props?.range?.[1] || undefined;
+
+    // query date order
+    if (isValidDate(fromDate) && isValidDate(toDate)) {
+      Object.assign(query, {
+        transaction_date_gteq: formatDate(fromDate, EDateFormat.DATE_API_QUERY),
+        transaction_date_lteq: formatDate(toDate, EDateFormat.DATE_API_QUERY),
+      });
+    }
+
+    const apiPayload: API.SearchTransactionQueryParams = {
+      items: props?.items || 1,
+      page: props?.page || 1,
+      query,
+    };
+    // fetch API
+    const rs = await transactionHistoryApis.search(apiPayload);
+
+    // fallback case
+    if (!rs?.data?.transaction_histories) {
+      return FALL_BACK_DRIVER_TRANSACTION_RESPONSE;
+    }
+
+    // success case
+    return {
+      transactionHistories: rs?.data?.transaction_histories || [],
+      totalPage: rs?.data?.total_page || 1,
+      currentPage: rs?.data?.current_page || 1,
+      totalRecord: rs?.data?.total_records || 0,
+    };
+  };
+  return {
+    // invoice
+    getDriverInvoices,
+    countDriverInvoices,
+    // transaction
+    getDriverTransaction,
+    countDriverTransactions,
   };
 };
