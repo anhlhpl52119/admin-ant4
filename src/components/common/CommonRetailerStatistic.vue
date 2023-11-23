a556a3f5502ef3f1fe84a556bc98185c8113f1fd
Hung Anh <anhlhpl52119@gmail.com>
2023-11-24 00:09:08 +0700

    chore: clean up

--- src/components/common/CommonRetailerStatistic.vue
+++ src/components/common/CommonRetailerStatistic.vue
@@ -1,30 +1,54 @@
 <template>
   <div class="grid grid-cols-2 gap-12 md:grid-cols-4">
-    <div class="card m-0 flex flex-col justify-between">
-      <div class="line-clamp-2">
-        <h3>Hóa đơn mới</h3>
+    <template v-for="item in renderItem" :key="item.id">
+      <div class="card m-0 flex flex-col justify-between" :class="item.className">
+        <div class="line-clamp-2">
+          <h3 class="text-16 font-500">
+            {{ item.name }}
+          </h3>
+        </div>
+        <ul class="flex-btw-center mt-auto">
+          <li>
+            <i class="block text-28" :class="item.icon" />
+          </li>
+          <li class="text-18 font-800">
+            {{ item.value }}
+          </li>
+        </ul>
       </div>
-      <div class="text-18 font-800 ml-auto">
-        125
-      </div>
-    </div>
-    <div class="card m-0 flex flex-col justify-between">
-      <div class="line-clamp-2">
-        <h3>Đợt thanh toán đang đợi</h3>
-      </div>
-      <div class="text-18 font-800 ml-auto">
-        <a>15</a>
-      </div>
-    </div>
-    <div class="card m-0">
-      <span> Tổng hóa đơn</span>
-    </div>
-    <div class="card m-0">
-      <span> Tổng hóa đơn</span>
-    </div>
+    </template>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+const renderItem = reactive([
+  {
+    id: '1',
+    name: 'Hóa đơn mới',
+    icon: 'i-basil:invoice-solid',
+    className: 'bg-#ecf3ff text-#8190e9',
+    value: '125',
+  },
+  {
+    id: '2',
+    name: 'Phiếu thanh toán đang chờ',
+    icon: 'i-tdesign:undertake-transaction',
+    className: 'bg-#fef5e6 text-#f2bd13',
+    value: '50',
+  },
+  {
+    id: '3',
+    name: 'Tài xế',
+    icon: 'i-game-icons:steering-wheel',
+    className: 'bg-#e8f7ff',
+    value: '50',
+  },
+  {
+    id: '4',
+    name: 'Chi nhánh',
+    icon: 'i-ic:round-store-mall-directory',
+    className: 'bg-#e5fffb text-#3cdbb3',
+    value: '2',
+  },
+]);
 </script>
--- src/views/retailer/dashboard/retailer-dashboard-overview.vue
+++ src/views/retailer/dashboard/retailer-dashboard-overview.vue
@@ -3,15 +3,15 @@
     <div class="grid grid-rows-3 grid-flow-col gap-10">
       <CommonRetailerStatistic class="row-span-3" />
       <div class="card m-0 col-span-2">
-        Chart
+        <!-- Chart -->
       </div>
       <div class="card m0 row-span-3">
-        it quan trong
+        <!-- item quan trong -->
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+const log = '';
 </script>
