2a0d315beeb3af8df61f71b0a3a17130ac0ccbb6
Hung Anh <anhlhpl52119@gmail.com>
2023-11-27 20:58:55 +0700

    chore: unix time util

--- src/utils/date.util.ts
+++ src/utils/date.util.ts
@@ -2,20 +2,47 @@ import type { Dayjs } from 'dayjs';
 import dayjs from 'dayjs';
 import { EDateFormat } from '@/enums/common.enum';
 
+export const isValidDate = (date: OrNullish<string | number | Dayjs>) => {
+  return dayjs(date).isValid();
+};
+
 export const formatDate = (date: OrNullish<string | number | Dayjs>, formatType: `${EDateFormat}` = EDateFormat.DATE_HOUR_MINUTE) => {
-  if (!dayjs(date).isValid()) {
+  if (!isValidDate(date)) {
     return 'Ngày không hợp lệ';
+  }
+
+  // unix time
+  if (typeof date === 'number' && date.toString().length === 10) {
+    return dayjs.unix(date).format(formatType);
   }
 
   return dayjs(date).format(formatType);
 };
 
 export const toUnixTime = (date: OrNullish<string | number | Dayjs>) => {
-  if (!dayjs(date).isValid()) {
+  if (!isValidDate(date)) {
     return 0;
   }
 
-  return Number(dayjs(date).format('X'));
+  return dayjs(date).unix();
+};
+
+export const toDayJs = (date: OrNullish<string | number | Dayjs>): Dayjs => {
+  if (typeof date === 'number' && date.toString().length === 10) {
+    return dayjs.unix(date);
+  }
+
+  return dayjs(date);
+};
+
+export const dateGap = (from: OrNullish<string | number | Dayjs>, to: OrNullish<string | number | Dayjs>,
+  period: 'day' | 'month' | 'year' = 'day'): OrNull<number> => {
+  const fromDate = toDayJs(from);
+  const toDate = toDayJs(to);
+  if (!(isValidDate(fromDate) && isValidDate(toDate))) {
+    return null;
+  }
+  return fromDate.diff(toDate, period);
 };
 
 export const timeFromNow = (date: OrNullish<string | number | Dayjs>) => {
@@ -31,8 +58,4 @@ export const isFiveMinutesAgo = (date: OrNullish<string | number | Dayjs>) => {
   }
   const fiveMinutesAgo = dayjs().subtract(5, 'minute');
   return dayjs(date).isSameOrAfter(dayjs(fiveMinutesAgo));
-};
-
-export const isValidDate = (date: OrNullish<string | number | Dayjs>) => {
-  return dayjs(date).isValid();
 };
