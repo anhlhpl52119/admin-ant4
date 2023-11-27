0c89b456b8d768781f64824e79ad286fcd201dcc
Hung Anh <anhlhpl52119@gmail.com>
2023-11-27 20:58:33 +0700

    chore: chart color

--- src/constants/common.constant.ts
+++ src/constants/common.constant.ts
@@ -60,3 +60,26 @@ export const CALENDAR_LABELS = {
   MONTH_FROM_1_TO_6: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6'],
   MONTH_FROM_7_TO_12: ['Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'],
 };
+
+const data = {
+
+  backgroundColor: [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 205, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(201, 203, 207, 0.2)',
+  ],
+  borderColor: [
+    'rgb(255, 99, 132)',
+    'rgb(255, 159, 64)',
+    'rgb(255, 205, 86)',
+    'rgb(75, 192, 192)',
+    'rgb(54, 162, 235)',
+    'rgb(153, 102, 255)',
+    'rgb(201, 203, 207)',
+  ],
+
+};
