51d5d0731d8f8d7f2eac03d72b8757c8844882c1
Hung Anh <anhlhpl52119@gmail.com>
2023-11-27 20:58:46 +0700

    chore: date format

--- src/enums/common.enum.ts
+++ src/enums/common.enum.ts
@@ -13,9 +13,14 @@ export enum ERole {
 
 export enum EDateFormat {
   DATE_HOUR_MINUTE = 'DD-MM-YYYY HH:mm',
+  DAY_AND_MONTH = 'DD-MM',
+  MONTH_AND_YEAR = 'MM-YYYY',
+  ONLY_YEAR = 'YYYY',
+  ONLY_DAY = 'DD',
   DATE_HOUR_MINUTE_SECOND = 'DD-MM-YYYY HH:mm:ss',
   DATE_API_QUERY = 'DD-MM-YYYY',
   UNIX_TIME = 'X',
   UNIX_TIME_SECOND = 'x',
   HOUR_AND_MINUTES = 'HH:MM',
+  MONTH_NAME = 'MMM',
 }
