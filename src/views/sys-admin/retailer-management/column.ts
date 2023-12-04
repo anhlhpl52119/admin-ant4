74079a22d4daa73b4c316a0dc3844726d11e8ad6
Hung Anh <anhlhpl52119@gmail.com>
2023-12-05 00:17:36 +0700

    chore: clean up

--- src/views/sys-admin/retailer-management/column.ts
+++ src/views/sys-admin/retailer-management/column.ts
@@ -1,57 +1,6 @@
 import type { TableColumnType } from 'ant-design-vue';
 import type { QueriesRaw } from '@/composable/useTableMethod';
 
-export const columns: TableColumnType<API.Retailer>[] = [
-  {
-    title: 'STT',
-    dataIndex: 'indexNum',
-    width: '7rem',
-    align: 'center',
-    fixed: 'left',
-  },
-  {
-    title: 'Tên',
-    dataIndex: 'name',
-    fixed: 'left',
-    ellipsis: true,
-  },
-  {
-    title: 'Mã',
-    dataIndex: 'retailer_code',
-    ellipsis: true,
-  },
-  {
-    title: 'Số điện thoại',
-    dataIndex: 'phone',
-    ellipsis: true,
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    ellipsis: true,
-  },
-  {
-    title: 'Địa chỉ',
-    dataIndex: 'address',
-    ellipsis: true,
-  },
-  {
-    title: 'Mô tả',
-    dataIndex: 'description',
-    ellipsis: true,
-  },
-  {
-    title: 'Sync status',
-    dataIndex: 'sync_status',
-    ellipsis: true,
-  },
-  {
-    dataIndex: 'edit',
-    width: '6rem',
-    fixed: 'right',
-  },
-];
-
 export const searchFilterRaw: QueriesRaw<API.Retailer>[] = [
 
   {
