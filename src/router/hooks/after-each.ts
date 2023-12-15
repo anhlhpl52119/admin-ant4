65753512345b72fdd61ad489e356ff7ee9169c50
Hung Anh <anhlhpl52119@gmail.com>
2023-12-15 21:53:08 +0700

    chore: update todo

--- src/router/hooks/after-each.ts
+++ src/router/hooks/after-each.ts
@@ -1,14 +1,11 @@
 import { type Router } from 'vue-router';
 
 // import NProgress from 'nprogress';
-
 // https://www.npmjs.com/package/nprogress
 
 export const afterEach = (router: Router) => {
   router.afterEach((to, from) => {
     if (to.path !== from.path) {
-      // TODO: add NProgress done
-    // NProgress.done(); // End progress bar
       document.documentElement.scrollTop = 0;
     }
   });
